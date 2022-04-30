be1e97560dc6676d1b43fa06916a06ba7f28e2f3
Dakuro <51865183+Dakuro@users.noreply.github.com>
2022-04-30 20:41:10 +0200

    Added package-info.java

---
+++ src/main/java/net/dakuro/arkhesys/package-info.java
@@ -0,0 +1,9 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+@FieldsAreNonnullByDefault
+package net.dakuro.arkhesys;
+
+import net.minecraft.FieldsAreNonnullByDefault;
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
